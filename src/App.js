70e798b24e4cc0a4d782fa1e00210f1cf3185f6d
Daniel Kim <dannykim200@gmail.com>
2020-06-27 20:39:50 -0400

    Styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api'; // automatically searches for index.js file in api folder
 import './App.css';
-import logo from './logo.svg';
+
+import covidImage from './images/image.png';
 
 
 
@@ -15,27 +16,29 @@ class App extends React.Component {
 
   }
 
+  // makes request to fetchData
   async componentDidMount() {
     const fetchedData = await fetchData();
     this.setState({ data: fetchedData });
   }
 
+  // fetches data for specific country retrieved from CountryPicker nativeselect option
   handleCountryChange = async (country) => {
     // fetch the data
     const fetchedData = await fetchData(country);
-
 
     // set the state
     this.setState({ data: fetchedData, country: country });
   }
 
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
     return (
       <div className={styles.container}>
+        <img className={styles.image} src={covidImage} alt="Covid-19"/>
         <Cards data={data}/>
         <CountryPicker handleCountryChange={this.handleCountryChange}/>
-        <Chart/>
+        <Chart data={data} country={country}/>
       </div>
     )
   }
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -3,7 +3,7 @@ import { fetchDailyData } from '../../api'
 import { Line, Bar } from 'react-chartjs-2';
 import styles from './Chart.module.css';
 
-const Chart = () => {
+const Chart = ({ data : { confirmed, recovered, deaths }, country }) => {
     // hook - dailyData is state, setDailyData is setState function
     const [dailyData, setDailyData] = useState([]);
 
@@ -12,7 +12,7 @@ const Chart = () => {
             setDailyData(await fetchDailyData()); // set dailyData to promise returned 
         }
         fetchAPI();
-    });
+    }, []);
 
     const lineChart = (
         // if dailyData is available, display chart
@@ -40,9 +40,33 @@ const Chart = () => {
         
     );
 
+    const barChart = (
+        confirmed
+         ? (
+             <Bar 
+               data={{
+                   labels: ['Infected', 'Recovered', 'Deaths'],
+                   datasets: [{
+                        label: 'People',
+                        backgroundColor: [
+                           'rgba(0, 0, 255, 0.5)',
+                           'rgba(0, 255, 0, 0.5)',
+                           'rgba(255, 0, 0, 0.5)',
+                        ],
+                        data: [confirmed.value, recovered.value, deaths.value]
+                   }]
+               }}
+               options={{
+                   legend: { display: false },
+                   title: { display: true, text: `Current state in ${country}` },
+               }}
+             /> 
+         ) : null
+    );
+
     return (
         <div className={styles.container}>
-            {lineChart}
+            {country ? barChart : lineChart }
         </div>
     )
 }
